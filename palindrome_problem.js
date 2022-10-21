7c9b476e9020e4ef09c798efbab5b255fca694d5
borsejugal <borsejugal@gmail.com>
2022-10-21 23:14:30 +0530

    problem for palindrome and check prime number

---
+++ palindrome_problem.js
@@ -0,0 +1,14 @@
+let str="divya";
+let nstr="";
+for(i=N-1;i>=0;i--){
+    nstr+=str[i];
+    
+}
+if(nstr==str){
+    console.log("No");
+}
+else{
+   console.log("Yes");
+}
+
+
---
+++ prime_problem.js
@@ -0,0 +1,13 @@
+let number = 13;
+let count=0;
+for (i=0;i<=13;i++){
+    if (number%i==0){
+        count++;
+        
+
+    }
+}if (count>=2){
+    console.log("it's a prime number");
+}else{
+    console.log("it not a prime number")
+}
